bc26754d3d140abbd5a55a1eb23c70e60f7d825a
mkrsym1 <mkrsym1@gmail.com>
2023-12-26 23:24:37 +0200

    Implemented periodic writes, command line parsing

--- src/main.c
+++ src/main.c
@@ -1,26 +1,54 @@
 #include <windows.h>
+#include <stdio.h>
 #include <game.h>
 #include <msg.h>
-#include <bytes.h>
-#include <utils.h>
 
-int main(int argc, char **argv) {
+void unlock_fps(int32_t target_fps, int64_t interval) {
     HANDLE game = open_game_process();
-
     if (!game) {
-        msg_err_a("Game process not found");
+        msg_err_a("Game process not found. The game has to be running before you start the unlocker");
     }
 
     uint32_t *fps = find_fps_var(game);
-    
-    msg_info_a("fps var: %p", fps);
-
-    uint32_t target_fps = 144;
     if (!WriteProcessMemory(game, fps, &target_fps, sizeof(target_fps), NULL)) {
         msg_err_a("Failed to write FPS: %d", GetLastError());
     }
 
-    msg_info_a("set fps");
+    if (interval > 0) {
+        while (WriteProcessMemory(game, fps, &target_fps, sizeof(target_fps), NULL)) {
+            Sleep(interval);
+        }
+    }
+}
+
+int main(int argc, char **argv) {
+    int32_t target_fps;
+    int64_t interval = 5000;
+
+    switch (argc) {
+        case 3:
+            interval = atoi(argv[2]);
+
+        case 2: 
+            target_fps = atoi(argv[1]);
+            break;
+
+        default:
+            printf("Usage: wine fpsunlock.exe [FPS] <interval>\n");
+            return 1;
+    }
+
+    if (target_fps < 1) {
+        printf("Invalid target FPS value\n");
+        return 1;
+    }
+
+    if (interval == 0) {
+        printf("Invalid interval value. Set a negative value to only write once\n");
+        return 1;
+    }
+
+    unlock_fps(target_fps, interval);
 
     return 0;
 }
