82adf7b9d8be46246b9649a8172110a96088b3aa
mkrsym1 <mkrsym1@gmail.com>
2023-12-27 14:10:33 +0200

    Marked msg_err functions noreturn

--- include/msg.h
+++ include/msg.h
@@ -2,8 +2,8 @@
 
 #include <wchar.h>
 
-void msg_err_a(const char *format, ...);
-void msg_err_w(const wchar_t *format, ...);
+void __attribute__((noreturn)) msg_err_a(const char *format, ...);
+void __attribute__((noreturn)) msg_err_w(const wchar_t *format, ...);
 
 void msg_warn_a(const char *format, ...);
 void msg_warn_w(const wchar_t *format, ...);
