841480d649df808f7d26a4b01e104cc5015ce642
mkrsym1 <mkrsym1@gmail.com>
2024-07-18 14:09:29 +0300

    Updated search logic

--- include/bytes.h
+++ include/bytes.h
@@ -3,7 +3,7 @@
 #include <windows.h>
 #include <stdint.h>
 
-#define ANY_ -1
+#define ANY_ (-1)
 
 void *find_pattern(void *address, size_t limit, int16_t *pattern, size_t length);
 void *find_pattern_ex(HANDLE process, void *address, size_t limit, int16_t *pattern, size_t length);
--- src/game.c
+++ src/game.c
@@ -44,78 +44,32 @@ HANDLE open_game_process() {
     return process;
 }
 
-HMODULE get_module(HANDLE process, const wchar_t *name) {
-    MODULEENTRY32W entry;
-    entry.dwSize = sizeof(entry);
-
-    HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, GetProcessId(process));
-
-    if (!Module32FirstW(snapshot, &entry)) {
-        msg_err_a("Could not enumerate modules: %d", GetLastError());
-    }
-
-    HMODULE module = NULL;
-    do {
-        if (wcsicmp(entry.szModule, name) == 0) {
-            module = entry.hModule;
-            break;
-        }
-    } while (Module32NextW(snapshot, &entry));
-
-    CloseHandle(snapshot);
-
-    return module;
-}
-
-HMODULE wait_for_module(HANDLE process, const wchar_t *name) {
-    HMODULE module = get_module(process, name);
-    while (!module) {
-        Sleep(10);
-        module = get_module(process, name);
-    }
-
-    return module;
-}
-
 uint32_t *find_fps_var(HANDLE process) {
-    HMODULE userassembly = wait_for_module(process, L"UserAssembly.dll");
+    HMODULE executable = (HMODULE)0x140000000; // not good but will work
 
     int16_t setter_call_pattern[] = {
-        0xB9, 0x3C, 0x00, 0x00, 0x00,    // B9   3C000000    mov ecx, 60
-        0xFF, 0x15                       // FF15 ????????    call [setter_ptr]
+        0xB9, 0x3C, 0x00, 0x00, 0x00,    // B9 3C000000    mov ecx, 60
+        0xE8, ANY_, ANY_, ANY_, ANY_,    // E8 ????????    call setter
+        0x80                             // 80 ?           cmp byte [x], y
     };
-    uint8_t *setter_call = find_pattern_ex_in_module(process, userassembly, IMAGE_SCN_MEM_EXECUTE, setter_call_pattern, COUNT(setter_call_pattern));
+    uint8_t *setter_call = find_pattern_ex_in_module(process, executable, IMAGE_SCN_MEM_EXECUTE, setter_call_pattern, COUNT(setter_call_pattern));
     if (!setter_call) {
         msg_err_a("Could not find setter call");
     }
 
-    int32_t setter_ptr_offset;
-    TRY_READ_MEMORY(process, setter_call + 7, &setter_ptr_offset, sizeof(setter_ptr_offset));
-
-    uint8_t *setter_ptr = setter_call + 11 + setter_ptr_offset;
-    
-    uint8_t *setter_addr = NULL;
-    TRY_READ_MEMORY(process, setter_ptr, &setter_addr, sizeof(setter_addr));
-
-    // Wait for setter addr if it isn't there yet
-    while (!setter_addr) {
-        Sleep(10);
-        TRY_READ_MEMORY(process, setter_ptr, &setter_addr, sizeof(setter_addr));
-    }
-
-    // Traverse jump chain
+    // Traverse chain
     uint8_t bytes_at_addr[6];
-    uint8_t *potential_mov = setter_addr;
+    uint8_t *potential_mov = setter_call + 5;
     while (1) {
         TRY_READ_MEMORY(process, potential_mov, bytes_at_addr, sizeof(bytes_at_addr));
 
-        if (bytes_at_addr[0] == 0xE9) { // jmp
+        if (bytes_at_addr[0] == 0xE9 || bytes_at_addr[0] == 0xE8) { // jmp / call
             potential_mov += *((int32_t*)(bytes_at_addr + 1)) + 5;
         } else {
             break;
         }
     }
-
+    
     // 890D ????????    mov [fps], ecx
     if (bytes_at_addr[0] != 0x89 || bytes_at_addr[1] != 0x0D) {
         msg_err_a("Could not find 'mov [fps], ecx'");
